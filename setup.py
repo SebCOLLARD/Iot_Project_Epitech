284ffbacc27c15fb6afcb3ac0c47a15142a69cde
Matthieu Rochette <matthieu.rochette@epitech.eu>
2021-11-21 01:20:35 +0100

    fix setup.py (json config missing in build folder)

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
+from os import getcwd
+from os.path import abspath, dirname, join
+
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-build_options = {'packages': [], 'excludes': []}
+build_options = {"include_files": ["./PyOneDark_settings.json"]}
 
 import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+
+base = "Win32GUI" if sys.platform == "win32" else None
 
 executables = [
-    Executable('main.py', base=base, target_name = 'iot_device_manager.exe')
+    Executable(
+        "main.py", base=base, target_name="iot_device_manager.exe", icon="icon.ico"
+    )
 ]
 
-setup(name='IoTDeviceManager',
-      version = '0.1',
-      description = 'This is a graphical interface for the IoTDeviceManager of the IoT Project for Epitech 4th year Global Nomad Track, 2021-2022 (promotion 2023).',
-      options = {'build_exe': build_options},
-      executables = executables)
+setup(
+    name="IoTDeviceManager",
+    version="0.1",
+    description="This is a graphical interface for the IoTDeviceManager of the IoT Project for Epitech 4th year Global Nomad Track, 2021-2022 (promotion 2023).",
+    options={"build_exe": build_options},
+    executables=executables,
+)
