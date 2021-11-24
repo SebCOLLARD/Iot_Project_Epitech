c122c620500abcdd5bfb26ed1c719bdcb1986297
Matthieu Rochette <matthieu.rochette@epitech.eu>
2021-11-24 18:01:16 +0100

    added logging properly

--- DeviceManager/config.py
+++ DeviceManager/config.py
@@ -1,4 +1,5 @@
 from os import getenv
+import logging
 
 from dotenv import load_dotenv
 
@@ -6,3 +7,6 @@ load_dotenv()
 
 THINGSBOARD_URL = getenv("THINGSBOARD_URL", "thingsboard.matthieu-rochette.fr")
 COAP_PORT = int(getenv("COAP_PORT", 5683))
+LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
+
+logging.basicConfig(level=LOG_LEVEL)
